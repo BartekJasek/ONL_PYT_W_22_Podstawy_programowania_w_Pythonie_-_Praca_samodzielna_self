683b7c294fbb690d4d826f7e167ed3e7e1def848
BartekJasek <bartek.jasek5@gmail.com>
2022-09-10 15:56:06 +0200

    02_Wartosci_logiczne

---
+++ 02_Typy_danych_w_Pythonie/01_Typy_danych/task.py
@@ -0,0 +1,13 @@
+int = 9 // 3
+result = "Zmienna int ma wartość {}".format(int)
+float = 10 / 3
+result2 = "Zmienna float ma wartość {}".format(float)
+company = 'Hello World'
+result3 = "Zmienna company ma wartość {}".format(company)
+is_done = True
+result4 = "Zmienna is_done ma wartość {}".format(is_done)
+
+print(result)
+print(result2)
+print(result3)
+print(result4)

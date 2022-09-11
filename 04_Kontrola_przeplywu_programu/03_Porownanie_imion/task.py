0350961c51d195b68fa9bb87bc26213733de9d31
BartekJasek <bartek.jasek5@gmail.com>
2022-09-11 13:33:51 +0200

    03_Porownanie_imion

---
+++ 04_Kontrola_przeplywu_programu/03_Porownanie_imion/task.py
@@ -0,0 +1,10 @@
+print("Podaj pierwsze imię:")
+first_name = input()
+print("Podaj drugie imię:")
+second_name = input()
+if first_name == second_name:
+    print("Takie same")
+elif first_name != second_name:
+    print("Różne")
+else:
+    pass

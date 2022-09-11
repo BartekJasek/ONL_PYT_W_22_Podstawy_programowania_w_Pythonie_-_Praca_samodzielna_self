6518bfe0d34aed75db61c0d11105dbc7825ff0c3
BartekJasek <bartek.jasek5@gmail.com>
2022-09-11 13:28:21 +0200

    02_Kolejne_potegi

---
+++ 04_Kontrola_przeplywu_programu/02_Kolejne_potegi/task.py
@@ -0,0 +1,23 @@
+result = [2]
+for element in result:
+    print(f"2 do potęgi 0 to {element**0}")
+for element in result:
+    print(f"2 do potęgi 1 to {element**1}")
+for element in result:
+    print(f"2 do potęgi 2 to {element**2}")
+for element in result:
+    print(f"2 do potęgi 3 to {element**3}")
+for element in result:
+    print(f"2 do potęgi 4 to {element**4}")
+for element in result:
+    print(f"2 do potęgi 5 to {element**5}")
+for element in result:
+    print(f"2 do potęgi 6 to {element**6}")
+for element in result:
+    print(f"2 do potęgi 7 to {element**7}")
+for element in result:
+    print(f"2 do potęgi 8 to {element**8}")
+for element in result:
+    print(f"2 do potęgi 9 to {element**9}")
+for element in result:
+    print(f"2 do potęgi 10 to {element**10}")

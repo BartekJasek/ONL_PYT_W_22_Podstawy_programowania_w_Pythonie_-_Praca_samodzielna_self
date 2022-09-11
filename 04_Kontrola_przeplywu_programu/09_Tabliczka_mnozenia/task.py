34b67580bd57edf985b9a4a4eda3fbd57339929c
BartekJasek <bartek.jasek5@gmail.com>
2022-09-11 16:04:37 +0200

    09_Tabliczka_mnozenia

---
+++ 04_Kontrola_przeplywu_programu/09_Tabliczka_mnozenia/task.py
@@ -0,0 +1,16 @@
+print("Podaj liczbę n:")
+n = input()
+n = int(n)
+if n > 10:
+    print("Proszę podac liczbe od 1 do 10")
+else:
+    print(f"1 * {n} = {n*1}")
+    print(f"2 * {n} = {n*2}")
+    print(f"3 * {n} = {n*3}")
+    print(f"4 * {n} = {n*4}")
+    print(f"5 * {n} = {n*5}")
+    print(f"6 * {n} = {n*6}")
+    print(f"7 * {n} = {n*7}")
+    print(f"8 * {n} = {n*8}")
+    print(f"9 * {n} = {n*9}")
+    print(f"10 * {n} = {n*10}")
